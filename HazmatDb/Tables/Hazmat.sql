9513a7bbc5cf19359d79365e81da243b4ce05759
dhill <doughill1000@gmail.com>
2015-03-17 11:09:09 -0400

    Initital Commit

---
+++ HazmatDb/Tables/Hazmat.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Hazmat]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Trans#] FLOAT NULL, 
+    [Void] BIT NULL, 
+    [Date] DATETIME NULL, 
+    [Inititals] CHAR(2) NULL, 
+    [Material] VARCHAR(50) NULL, 
+    [New/Distilled/Dark] VARCHAR(10) NULL, 
+    [Quantity] FLOAT NULL, 
+    [Gallon/Pound] VARCHAR(10) NULL, 
+    [Destination] VARCHAR(50) NULL, 
+    [RequestedBy] CHAR(2) NULL, 
+    [Shift] VARCHAR(10) NULL, 
+    [RequiredFor] VARCHAR(15) NULL, 
+    [Notes] VARCHAR(255) NULL
+)
