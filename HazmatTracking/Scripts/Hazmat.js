6ab9a5eb8a0408385fabf50c80184dc3a7a43d72
Doug Hill <doug.hill@airdye.com>
2015-04-09 09:13:59 -0400

    Changed default datepicker date to month behind

---
+++ HazmatTracking/Scripts/Hazmat.js
@@ -0,0 +1,42 @@
+﻿//Allows the customer name to be submitted through the dropdown selection
+$(function () {
+
+    $("form").submit(function () {
+        var dropDownText = $("#hazmat_Material option:selected").text();
+        if (dropDownText.length > 0) {
+            $("#hazmat_Material").attr("value", dropDownText);
+        }
+    });
+
+    $("form").submit(function () {
+        var dropDownText = $("#hazmat_Destination option:selected").text();
+        if (dropDownText.length > 0) {
+            $("#hazmat_Destination").attr("value", dropDownText);
+        }
+    });
+
+    //Used for create pages. Image path is different than edit
+    $(".dateCreate").datepicker({
+        showOn: "button",
+        buttonImage: "../Images/Calendar.gif",
+        buttonImageOnly: true,
+        buttonText: "Select date",
+       defaultDate: '-1m'
+    });
+
+    //Used for edit pages. Image path is different than create
+    $(".dateEdit").datepicker({
+        showOn: "button",
+        buttonImage: "../../Images/Calendar.gif",
+        buttonImageOnly: true,
+        buttonText: "Select date",
+    });
+
+    //Formated datepicker date for editing mode
+    var date = new Date($("#hazmat_DateEdit").val());
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var day = date.getDate();
+    var formattedDate = month + '/' + day + '/' + year;
+    $("#hazmat_DateEdit").val(formattedDate);
+});
